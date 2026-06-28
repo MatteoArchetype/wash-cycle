2a85588802ecf4a7437585c088dc77bcc310d612
Matteo Maniscalco <matteo.maniscalco7@protonmail.com>
2026-06-28 21:56:51 +0200

    fix: remove deprecated appDir experimental flag for Next.js 16

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // Tell Next.js to use the App Router
-  experimental: {
-    appDir: true,
-  },
+  // Remove the entire experimental block
+  // appDir is now enabled by default in Next.js 16
 };
 
 module.exports = nextConfig;
